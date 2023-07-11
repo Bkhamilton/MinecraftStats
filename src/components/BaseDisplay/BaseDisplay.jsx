b90263fdb5a0887d56fde46e0106f715e92e3128
Bkhamilton <bkhamilton310@gmail.com>
2023-07-10 23:54:32 -0400

    New Changes

--- src/components/BaseDisplay/BaseDisplay.jsx
+++ src/components/BaseDisplay/BaseDisplay.jsx
@@ -17,6 +17,7 @@ const BaseDisplay = ({ node, onClick }) => {
   return (
     <div className="base-display-outer">
       <div className='base-display'>
+        {node.features.length === 0 && (<h1 className='display-title'><minecraft>{node.title}</minecraft></h1>)}
         <div className="buttons-container">
           {node.features.map((feature, index) => (
           <button
@@ -24,14 +25,22 @@ const BaseDisplay = ({ node, onClick }) => {
             className={`feature-button ${toggleStates[index] ? 'active' : ''}`}
             onClick={() => toggleButton(index, feature.type)}
           >
-            <div>
+            <div className='feature-button-inner'>
               <span>{feature.type}</span>
             </div>
           </button>
           ))}
         </div>
+        <div className='query-option-container'>
+          {node.options.map((option, index) => (
+            <div key={index} className='query-option'>
+              <div className='query-option-box'></div>
+              <p><minecraft>{option}</minecraft></p>
+            </div>
+          ))}
+        </div>
       </div>
-      <h1 className='display-title-test'><minecraft>{node.title}</minecraft></h1>
+      {node.features.length > 0 && (<h1 className='display-title-test'><minecraft>{node.title}</minecraft></h1>)}
     </div>
   );
 };
